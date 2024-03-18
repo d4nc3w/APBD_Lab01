d66ca060b1ef635eaaf7c4aca13bbc7ae6ffd063
s28113 <s28113@pjwstk.edu.pl>
2024-03-18 14:18:46 +0100

    Modification 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Insert your name: ");
+string name = Console.ReadLine();
 for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Hello, World!");
+    Console.WriteLine(i + ". Hello, " + name + "!");
 }
+
