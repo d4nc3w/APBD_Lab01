3d264a53cbbb75f0459d610dfd5c453c0777e8fc
s28113 <s28113@pjwstk.edu.pl>
2024-03-18 14:47:17 +0100

    Adding average method on feature-average branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Insert your name: ");
-string name = Console.ReadLine();
-for (int i = 0; i < 10; i++)
+public class Program
 {
-    Console.WriteLine(i + ". Hello, " + name + "!");
-}
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Insert your name: ");
+        string name = Console.ReadLine();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i + ". Hello, " + name + "!");
+        }
 
-Student student = new Student(name);
-Console.WriteLine(student.firstName);
+        Student student = new Student(name);
+        Console.WriteLine(student.firstName);
+        
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int result = averageNum(numbers);
+        Console.WriteLine("The average number is: " + result);
+    }
+    
+    public static int averageNum(int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        int result = sum / numbers.Length;
+        return result;
+    }
+}
 
 internal class Student
 {
@@ -19,6 +39,8 @@ internal class Student
         this.firstName = firstName;
     }
 }
+
+    
 
 
 
